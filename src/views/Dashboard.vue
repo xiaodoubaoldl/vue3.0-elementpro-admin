814d22f6b663e077720a7787b7a94a31d0798e40
lvdongliang <lvdongliang@cennavi.com.cn>
2021-07-21 20:22:42 +0800

    svg corner

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
+    <github-corner/>
     <h1>首页</h1>
   </div>
 </template>
 
 <script>
+import GithubCorner from '@/components/GithubCorner.vue';
 import { reactive, toRefs } from 'vue';
 
 export default {
+  components: {
+    GithubCorner,
+  },
   setup() {
     const state = reactive({
       count: 0,
