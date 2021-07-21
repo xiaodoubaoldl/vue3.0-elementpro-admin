162acf7d0e393c32e2d8c02040f4aab0dd4ed56c
lvdongliang <lvdongliang@cennavi.com.cn>
2021-07-21 19:57:22 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const resolve = (dir) => path.join(__dirname, dir);
 module.exports = {
+  publicPath: '/vue3.0-elementpro-admin/',
+  assetsDir: 'static',
   configureWebpack: {
     resolve: {
       alias: {
